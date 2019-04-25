cab03de1376f8fbaece0d9b45a12dad34ecba4fe
杨力 <944598111@qq.com>
2019-04-25 08:58:17 +0800

    初识Ribbon负载均衡

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer_80.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer_80.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Time: 22:18
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class,args);
