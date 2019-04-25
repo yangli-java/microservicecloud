4b26b3e3e2a0b91e13d625d7ad6a7bf4c0ac0c5a
杨力 <944598111@qq.com>
2019-04-25 17:48:41 +0800

    springcloud的Ribbon负载均衡算法的配置

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,9 @@
 package com.atguigu.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +22,18 @@ public class ConfigBean {
     @LoadBalanced   //开启自动负载均衡，即可以通过微服务名称进行调用微服务，不需要使用ip+端口号进行调用了。
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+
+    /**
+     * springcloud的Riboon已经配置了七种负载均衡的算法，它们之间可以相互切换。
+     * 这七种负载均衡算法都是 IRule 接口的实现类
+     * @return
+     */
+    @Bean
+    public IRule getIRule() {
+//        return  new RoundRobinRule();//简单轮询负载均衡，若没有配置该Bean，则Ribbon默认采取该种方式的负载均衡
+        return new RandomRule();//随机负载均衡
+//        return new RetryRule();//重试轮询负载均衡
     }
 }
